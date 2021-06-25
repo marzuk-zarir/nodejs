993186f508ff39082dffe817197dca9af1f01a83
marzuk(linux) <linux.marzukzarir@gmail.com>
2021-06-25 19:19:22 +0600

    folder structure change. install folder deleted

--- 1 - Nodejs Setup/setup.js
+++
@@ -1,6 +0,0 @@
-/**
- * download nodejs form https://nodejs.org
- * or
- * install with nvm(node version manager)
- * nvm download: https://github.com/coreybutler/nvm-windows#node-version-manager-nvm-for-windows
- */
